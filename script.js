6c7d930c0e04c4cb37bf5717ac7c63b80c1608d7
SHARAN MURLI <45029621+sharanmurli@users.noreply.github.com>
2023-02-28 20:59:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $('#container').addClass('loaded');
+    // Once the container has finished, the scroll appears
+    if ($('#container').hasClass('loaded')) {
+      // It is so that once the container is gone, the entire preloader section is deleted
+      $('#preloader').delay(1000).queue(function() {
+        $(this).remove();
+      });}
+  }, 3000);});
+
+  
+  $(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+  
+      $('.animated-icon1').toggleClass('open');
+    });
+   
+  });
+
+  $(document).ready(function(){
+    $('.toggle').click(function(){
+      $('.sidebar-contact').toggleClass('active')
+      $('.toggle').toggleClass('active')
+    })
+  })
+
+
+
+  const imgs = document.querySelectorAll('.img-select a');
+  const imgBtns = [...imgs];
+  let imgId = 1;
+  
+  imgBtns.forEach((imgItem) => {
+      imgItem.addEventListener('click', (event) => {
+          event.preventDefault();
+          imgId = imgItem.dataset.id;
+          slideImage();
+      });
+  });
+  
+  function slideImage(){
+      const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+  
+      document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+  }
+  
+  window.addEventListener('resize', slideImage);
+
+
+  //overlay products
+
+  
