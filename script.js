10792760c55ed8de9423fd6f5a49e6d54b896cd4
Sharan <agnimurli@gmail.com>
2023-03-15 14:53:32 +0530

    new update

--- script.js
+++ script.js
@@ -52,3 +52,17 @@ $(document).ready(function() {
   //overlay products
 
   
+$(document).ready(function(){ 
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 100) { 
+          $('#scroll').fadeIn(); 
+      } else { 
+          $('#scroll').fadeOut(); 
+      } 
+  }); 
+  $('#scroll').click(function(){ 
+      $("html, body").animate({ scrollTop: 0 }, 300); 
+      return false; 
+  }); 
+});
+  
